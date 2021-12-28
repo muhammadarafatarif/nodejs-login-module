c10400ff6e33f109faa57a8a3dac0ad5d24f9ad1
Muhammad Arafat <muhammad.arafat.arif@gmail.com>
2021-12-28 15:33:26 +0500

    initialized and set up our server

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+
+// set up port number
+const port = 5035;
+
+// set up home route
+app.get('/', (request, respond) => {
+  respond.status(200).json({
+    message: 'Welcome to Project Support',
+  });
+});
+
+app.listen(port, (request, respond) => {
+    console.log(`Our server is live on ${port}. Yay!`);
+});
